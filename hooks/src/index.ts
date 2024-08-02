6e714af7f85ffdeee4613e44d6303722d348948a
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-08-02 22:15:22 +0530

    prisma update

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -6,39 +6,31 @@ const client = new PrismaClient();
 const app = express();
 app.use(express.json());
 
+// https://hooks.zapier.com/hooks/catch/17043103/22b8496/
+// password logic
 app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
   const body = req.body;
 
-  try {
-    // store in db a new trigger
-    await client.$transaction(async (tx) => {
-      const run = await tx.zapRun.create({
-        data: {
-          zapId: zapId,
-          metadata: body,
-        },
-      });
-
-      await tx.zapRunOutbox.create({
-        data: {
-          zapRunId: run.id,
-        },
-      });
+  // store in db a new trigger
+  await client.$transaction(async (tx) => {
+    const run = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body,
+      },
     });
 
-    res.json({
-      message: "Webhook received",
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
     });
-  } catch (error) {
-    console.error("Error processing webhook:", error);
-    res.status(500).json({
-      message: "Internal server error",
-    });
-  }
+  });
+  res.json({
+    message: "Webhook received",
+  });
 });
 
-app.listen(3002, () => {
-  console.log("Server is running on port 3002");
-});
+app.listen(3002);
